81b8e1553c8d151d39e01670ce98bbd55401141e
Alejandra2254 <2254@holbertonschool.com>
2021-03-30 14:59:28 -0500

    Adding modifications on city and init file

--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+import sqlalchemy
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+   """ The city class, contains state ID and name """
+   __tablename__ = 'cities'
+   
+   if getenv('HBTN_TYPE_STORAGE') == 'db':
+      name = Column(String(128), nullable=False)
+      state_id = Column(String(60), ForeignKey('States.id'), nullable=False)
+      places = relationship('Place', cascade='all, delete', backref='cities')
+   else:
+      name = ""
+      state_id = ""
+
