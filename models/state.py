9a2747e59b73e67a9c4e51390e0e0af7f95d0efe
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-30 20:07:09 +0000

    Model Update

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if getenv("HBNB_TYPE_STORAGE") == "db"
+        name = Colum(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all,delete')
+    else:
+        @property
+        def cities(self):
+        """Getter for cities from the same state"""
+        self_cities = []
+        for key, val in models.storage.all().items():
+            if val.__class__.__name__ == 'City'
+                    and val.state._id == self.id:
+                self_cities.append(val)
+        return self_cities
