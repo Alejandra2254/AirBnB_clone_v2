4681ecef01a18c933cb9c0f3ef465f48228d49ec
Alejandra2254 <2254@holbertonschool.com>
2021-03-30 22:23:23 -0500

    adding skip test with just pass

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+from os import getenv
 
 
 class test_state(test_basemodel):
@@ -12,6 +13,16 @@ class test_state(test_basemodel):
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
+    
+    @unittest.skip("demonstrating skipping")
+    def test_nothing(self):
+        self.fail("shouldn't happen")
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db",
+                     "not supported in this library version")
+    def test_format(self):
+        # Tests that work for only a certain version of the library.
+        pass
 
     def test_name3(self):
         """ """
