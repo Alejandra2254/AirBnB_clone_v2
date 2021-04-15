4c457f787ec46ed39274157204ec06ba58f7a0d5
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-04-15 13:19:50 -0500

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,6 @@ import datetime
 
 
 def do_pack():
-    """generates a .tgz archive from the contents of the web_static folder"""
     try:
         now = datetime.datetime.now()
         date = now.strftime('%Y%m%d%H%M%S')
