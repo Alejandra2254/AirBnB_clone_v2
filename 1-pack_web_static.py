5731cd7851f00edd200e124e23162e9dec3df5e0
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-04-15 13:11:02 -0500

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,8 +9,8 @@ def do_pack():
     try:
         now = datetime.datetime.now()
         date = now.strftime('%Y%m%d%H%M%S')
-        name = "web_static_" + date + ".tgz"
-        local("tar -cvfz versions/{} web_static".format(name))
+        name = "versions/web_static_" + date + ".tgz"
+        local("tar -cvfz {} web_static".format(name))
         return name
     except:
         return None
