2d26e9b3f945263ded122ffb6a5774d5ba3d213d
Alejandra2254 <2254@holbertonschool.com>
2021-04-15 11:09:01 -0500

    Adding set up web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+sudo apt-get update -y
+sudo apt-get install nginx -y 
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+echo "My Web Page Holberton" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "38i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default
+sudo service nginx restart
