4c679f175338adb2fd6512b5bdac6b4aa58d91c6
Alejandra2254 <2254@holbertonschool.com>
2021-05-02 19:27:09 -0500

    Adding task 3 using defaults variables

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,8 +21,7 @@ def c(text):
     """Function HBNB"""
     return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/python', defaults={"text": "is cool"},
-           strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     """Function HBNB"""
