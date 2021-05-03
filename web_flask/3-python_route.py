956e0cbf8180d8737d2813010b8a6f8c05f80863
Alejandra2254 <2254@holbertonschool.com>
2021-05-02 19:27:31 -0500

    fix by pep8 style

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c(text):
     """Function HBNB"""
     return 'C {}'.format(text.replace('_', ' '))
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
