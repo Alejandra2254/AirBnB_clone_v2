b5e77e95ec408f3a19c8725fccc2e41396bc6f4e
Alejandra2254 <2254@holbertonschool.com>
2021-05-02 21:03:31 -0500

    ADDing fix style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,7 +56,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-    
+
     def delete(self, obj=None):
         '''delete an specified obj from __objects'''
         if obj is not None:
