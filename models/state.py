46d37299c7b9a9bd706827c749856a5b2ad4e9e3
Alejandra2254 <2254@holbertonschool.com>
2021-05-02 21:01:11 -0500

    Adding state

--- models/state.py
+++ models/state.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from os import getenv
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """State class"""
     __tablename__ = 'states'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all,delete')
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
     else:
         @property
         def cities(self):
-            """Getter for cities from the same state"""
-            self_cities = []
-            for key, val in models.storage.all().items():
-                if val.__class__.__name__ == 'City' and val.state._id == self.id:
-                    self_cities.append(val)
-            return self_cities
+            '''getter for cities'''
+            from models import storage
+            list_cities = []
+            cities_dict = storage.all(City)
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
+
