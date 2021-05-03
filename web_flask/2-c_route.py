fe18d746912bd825268bc19482b10177c45e06fc
Alejandra2254 <2254@holbertonschool.com>
2021-05-02 19:14:48 -0500

    Addign task 2 using variables tect

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Working with Flask Module'''
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Function HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Function HBNB"""
+    return 'C ' % escape(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
