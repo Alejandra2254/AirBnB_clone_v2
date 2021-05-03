c8998757ef4bb06d6ad41a03e445ad27b734c13d
Alejandra2254 <2254@holbertonschool.com>
2021-05-02 19:26:10 -0500

    Adding task 3 using defaults variables

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Working with Flask Module'''
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Function HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Function HBNB"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python', defaults={"text": "is cool"},
+           strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """Function HBNB"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
